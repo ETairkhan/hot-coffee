03cb79c13ce41136130b44a4400a6913fe179142
ayzhunis <sabdpp17@gmail.com>
2025-03-17 19:12:52 +0500

    initizialize the structure of project

---
+++ cmd/hot-coffee/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "flag"
+
+var (
+	port int
+	dir string
+)
+
+func init() {
+	flag.IntVar(&port, "port", 8080, "port number.")
+	flag.StringVar(&dir, "dir", "data", "Path to the data directory")
+}
+
+func main() {
+	flag.Parse()
+
+	
+}
